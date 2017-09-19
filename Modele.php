42c9bb87ba281f244cebfdad2c42b664a0c7f4e6
mitden <timothee.nedelec@gmail.com>
2017-09-19 16:25:04 +0200

    suppression de billet.php

--- Modele.php
+++ Modele.php
@@ -1,4 +1,11 @@
 <?php
+// Effectue la connexion à la BDD
+// Instancie et renvoie l'objet PDO associé
+function getBdd() {
+	$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8',
+			'ts2', 'ts2', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	return $bdd;
+}
 
 // Renvoie la liste de tous les billets, triés par identifiant décroissant
 function getBillets() {
@@ -9,9 +16,57 @@ function getBillets() {
 	return $billets;
 }
 
-// Effectue la connexion à la BDD
-// Instancie et renvoie l'objet PDO associé
-function getBdd() {
-	$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'ts2', 'ts2', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	return $bdd;
+
+// Renvoie les informations sur un billet
+function getBillet($idBillet) {
+	$bdd = getBdd();
+	$billet = $bdd->prepare('select BIL_ID as id, BIL_DATE as date,'
+			. ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+			. ' where BIL_ID=?');
+	$billet->execute(array($idBillet));
+	if ($billet->rowCount() == 1)
+		return $billet->fetch();  // Accès à la première ligne de résultat
+		else
+			throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
 }
+
+
+
+
+
+// Renvoie la liste des commentaires associés à un billet
+function getCommentaires($idBillet) {
+	$bdd = getBdd();
+	$commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,'
+			. ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'
+			. ' where BIL_ID=?');
+	$commentaires->execute(array($idBillet));
+	return $commentaires;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
