1fc975670b50ee87ad9ef559a281659577cda584
mitden <timothee.nedelec@gmail.com>
2017-09-19 16:40:20 +0200

    v

---
+++ Controleur.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'Modele.php';
+
+// Affiche la liste de tous les billets du blog
+function accueil() {
+	$billets = getBillets();
+	require 'vueAccueil.php';
+}
+
+// Affiche les détails sur un billet
+function billet($idBillet) {
+	$billet = getBillet($idBillet);
+	$commentaires = getCommentaires($idBillet);
+	require 'vueBillet.php';
+}
+
+// Affiche une erreur
+function erreur($msgErreur) {
+	require 'vueErreur.php';
+}
